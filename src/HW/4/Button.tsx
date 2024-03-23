0ec84d96cbcb297054b15dfb9df8e3ea0c692547
Ruslan Rutkovsky <rus.rutkovsky@yandex.ru>
2024-03-23 10:56:35 +0300

    - complete task of HW4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,11 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-};
+  callBack: () => void
+  name: string
+}
 
 export const Button = (props: ButtonPropsType) => {
-  const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
-  };
-
+  const callBackHandler = () => props.callBack()
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
-    </button>
-  );
-};
+    <button id={'hw04-button'} onClick={callBackHandler}>{props.name}</button>
+  )
+}
